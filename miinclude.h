545628f279893bf77d3cd1f1a1ed06d1b5055476
Alejandro <alejandroquesadasanchez@gmail.com>
2019-03-04 18:41:23 +0100

    commit en ramaA

--- miinclude.h
+++ miinclude.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 void f()
 {
-	printf("Hello World \n");
+	char c1[100]= "Hola mundo";
+	char c2[100]= ", soy <aquí tu nombre>";
+	printf("%s\n", strcat(c1, c2));
 }
